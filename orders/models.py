963693998cb0ab942a22270af94e2be3e6d90106
cla-bit <doublep098@gmail.com>
2024-07-21 12:16:17 +0100

    Fix[added model classes in orders/models.py]

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from products.models import Product
+from apis.models import CustomUser
 
 # Create your models here.
+
+
+class Order(models.Model):
+    user = models.ForeignKey(CustomUser, related_name='orders', on_delete=models.CASCADE)
+    paid = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now=True, null=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return f"Order ID: {self.id}"
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
